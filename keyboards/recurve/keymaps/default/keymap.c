ee4667c2d3ea34207b2a78e3b6e0240ba71cd0d0
Peter Witt <peter@Peters-MacBook-Pro.local>
2019-09-07 15:00:56 +0200

    Worked on my layout

--- keyboards/recurve/keymaps/default/keymap.c
+++ keyboards/recurve/keymaps/default/keymap.c
@@ -31,41 +31,43 @@ enum custom_keycodes {
 
 #define XXXXXXX KC_NO
 #define _______ KC_TRNS
-#define KC_LABS LALT(KC_BSPC)
+#define LALTBS LALT(KC_BSPC)
+#define RUN LCTL(KC_R)
+#define SFTTAB LSFT(KC_TAB)
 
 uint8_t adjustCount;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
-    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,                             KC_7,     KC_8,    KC_9,    KC_0,   KC_MINS, KC_EQL,  KC_EQL,
-    KC_TAB,  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,    KC_ESC,         KC_ENT,  KC_Y,     KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC, KC_LBRC,
-    UTIL,    RAISE,   KC_A,    KC_S,    KC_D,    KC_F,   KC_G,    KC_TAB,         KC_ENT,  KC_H,     KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_QUOT,
-    KC_LSFT, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,    KC_ENT, KC_ENT, KC_ENT,  KC_N,     KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_UP,   KC_UP,
-    KC_LCTL, KC_LCTL, KC_LCTL, KC_LALT, KC_LGUI, LOWER,  KC_BSPC, KC_ENT, KC_ENT, KC_ENT,  KC_SPACE, KC_LSFT, KC_RALT, KC_APP, KC_LEFT, KC_DOWN, KC_DOWN
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,                               KC_7,     KC_8,    KC_9,    KC_0,   KC_MINS, KC_EQL,  KC_BSPC,
+    SFTTAB,  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,    KC_ESC,           KC_ENT,  KC_Y,     KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC,
+    UTIL,    RAISE,   KC_A,    KC_S,    KC_D,    KC_F,   KC_G,    KC_TAB,           KC_BSPC, KC_H,     KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_BSLS,
+    KC_LSFT, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,    RUN,     KC_PGUP, KC_DEL,  KC_N,     KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_UP,   KC_DEL,
+    KC_LCTL, KC_LCTL, KC_LCTL, KC_LALT, KC_LGUI, LOWER,  KC_BSPC, KC_HOME, KC_PGDN, KC_END,  KC_SPACE, KC_LSFT, KC_RCTL, KC_APP, KC_LEFT, KC_DOWN, KC_RGHT
   ),
-
   [_RAISE] = LAYOUT(
-    RESET,   RESET,   _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______, _______,
+
+    RESET,   _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______, LALTBS,
     _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, KC_PGUP, KC_UP,   KC_PGDN, _______, _______, _______,
-    _______, _______, KC_LSFT, KC_TAB,  KC_LALT, KC_LGUI, KC_LCTL, _______,          _______, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_BSPC, _______, _______,
+    _______, _______, KC_LSFT, KC_TAB,  KC_LALT, KC_LGUI, KC_LCTL, _______,          KC_DEL,  KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_BSPC, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_END,  KC_DEL,  _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, KC_LABS, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    _______, _______, _______, _______, _______, _______, LALTBS,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
   ),
 
   [_LOWER] = LAYOUT(
      _______, _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______, _______,
      KC_F1,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______,          _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_F12, 
      KC_1,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    _______,          _______, KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_EQL, 
-     _______, _______, _______, _______, _______, _______, KC_GRV,  _______, _______, _______, KC_COMM, KC_DOT,  KC_LBRC, KC_RBRC, KC_BSLS, _______, _______,
-     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+     _______, _______, _______, _______, _______, _______, KC_GRV,  _______, KC_VOLU, KC_MPLY, KC_COMM, KC_DOT,  KC_LBRC, KC_RBRC, KC_BSLS, _______, _______,
+     _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_VOLD, KC_MNXT, _______, _______, _______, _______, _______, _______, _______
   ),
-
+  
     [_UTIL] = LAYOUT(
-     _______, _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______, _______,
-     _______, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLU, _______,          _______, _______, _______, _______, _______, _______, _______, _______,
-     _______, _______, _______, _______, _______, _______, KC_VOLD, _______,          _______, _______, _______, _______, _______, _______, _______, _______,
-     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+     _______, _______, _______, _______, _______, _______, _______,                            KC_NLCK, KC_PSLS, KC_PAST, KC_PEQL, _______, _______, _______,
+     _______, _______, KC_BTN1, KC_MS_U, KC_BTN2, KC_WH_U, _______, _______,          KC_ACL0, KC_P7,   KC_P8,   KC_P9,   KC_PMNS, _______, _______, _______,
+     _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, _______, _______,          KC_ACL1, KC_P4,   KC_P5,   KC_P6,   KC_PPLS, _______, _______, _______,
+     _______, _______, KC_BTN4, KC_BTN3, KC_BTN5, KC_WH_L, KC_WH_R, _______, _______, KC_ACL2, KC_P1,   KC_P2,   KC_P3,   _______, _______, _______, _______,
+     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_P0,   KC_P0,   KC_PDOT, _______, _______, _______, _______
   )
 };
 
